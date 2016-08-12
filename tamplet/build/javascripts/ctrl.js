7d39405a9b495fddb675b863dd73f94f92a2db07
ilandy <ilandy@infbeyond.com>
2016-08-12 13:55:06 +0800

    更新完成切版的畫面

--- tamplet/build/javascripts/ctrl.js
+++ tamplet/build/javascripts/ctrl.js
@@ -4,8 +4,6 @@
 	angular
 		.module('myApp')
 		.controller('myCtrl',myCtrl);
-	
-
 	function myCtrl($http){
 		var vm = this; 
 		vm.caseActive = false;
@@ -13,38 +11,24 @@
 
 		vm.isActive = false;
 		vm.data = {};
-		vm.caseActive = getItemActive;
+		vm.caseActive = menuActive;
 		vm.itemActive;
 		vm.activeVal = true;
 		vm.normalVal = false;
 		vm.newsActive = true;
-		vm.getScrollTarget = gotoBottom;
 		vm.prevActive;
 
 		//sample js used
-		vm.pop = false;
+		vm.pop = function (){
+			setTimeOut(function(){
+				window.location.assign("query-detail.html");
+			},5000);
+			vm.pop =  true;
+		};
+
 		$http.get('javascripts/data.json').success(function(data) {
             vm.data = data;
-            //console.log(vm.data);
-            vm.item = data.proType1.concat(data.proType2).concat(data.proType3).concat(data.proType4);
-			vm.item1 = data.proType1;
-			vm.item2 = data.proType2;
-			vm.item3 = data.proType3;
-			vm.item4 = data.proType4;
-			vm.pType = 0;
-			vm.pData = {
-				"Name" : "",
-				"Pic": "",
-		 		"Discription": {},
-		 		"OtherImg": [],
-		 		"IsVisble": true,
-				"GetContentIndex": 0,
-			 	"GetContentClass": "col-sm-5"
-			};
-			vm.szData = data.szInfo;
-			vm.szImg = vm.szData[0].Pic;
-
-			vm.showSZ = showSZ;
+           
 			vm.menuActive = menuActive;
         }).error(function(data, status, headers, config) {
             console.log('error');
