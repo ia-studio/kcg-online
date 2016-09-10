6cbc969fbd8352c3d5c8efcf0633c9ffb91c1a5d
ilandy <ilandy@infbeyond.com>
2016-09-10 12:03:46 +0800

    版面以及動畫調整 	1. 調整 faq 的下拉選單 以及 icon 的位置 	2. 增加 news 滾滾滾的 CSS & Javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,27 @@ export class AppComponent {
     showNews = true;
     showSlideMenu = false;
     showPhoneButton = true;
-    public constructor() { }
+    newsCount = 0;
+
+    showNowNews (idx:number){
+      return (this.newsCount % this.newsList.length) === idx ;
+    }
+    public constructor() {
+      setInterval(() => { this.newsCount++ }, 3000)
+    }
+
+    newsList = [{
+      title: "01如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信",
+      content: "01如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信。"
+    },{
+      title: "02如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信",
+      content: "02如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信。"
+    },{
+      title: "03如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信",
+      content: "03如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信。"
+    },{
+      title: "04如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信",
+      content: "04如果您是用 Yahoo! 、Hinet 、PChome 等免費信箱，可以先至「垃圾信匣」找看看，有可能被誤判為垃圾信。"
+    }];
+
 }
