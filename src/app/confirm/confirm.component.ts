b1bfdda01150e597692ec8ebfe3a71ec7faa72f6
ilandy <w9874123@gmail.com>
2017-04-21 23:42:18 +0800

    完成確認信功能樣式

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -17,32 +17,27 @@ export class ConfirmComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) {
       this.querySet= window.location.href.split('?')[1].split('=');
       this.queryExist = (this.querySet[0]=='firstconfirm' || this.querySet[0]=='datetime' || this.querySet[0]=='caseNo' || this.querySet[0]== 'error')? true : false;
-
+      
   }
 
-
-  getQuery (){
-      this.activatedRoute.params.subscribe((params: Params) => {
-
-        if (this.queryExist) {
-            this.key = this.querySet[0];
-            this.val = this.querySet[1];
-        }
-
-      });
-  }
   getdate(){
     let date = this.val.split('',14);
-    let year = date[0]+date[1]+date[2]+date[3]+'年 ',
-        month = date[4]+date[5]+'月 ',
+    let year = date[0]+date[1]+date[2]+date[3]+'年',
+        month = date[4]+date[5]+'月',
         day = date[6]+date[7]+'日 ',
-        hour = date[8]+date[9]+'點 ',
-        min = date[10]+date[11]+'分 ',
-        sec = date[12]+date[13]+'秒 ';
+        hour = date[8]+date[9]+'點',
+        min = date[10]+date[11]+'分',
+        sec = date[12]+date[13]+'秒';
     return year+month+day+hour+min+sec;
   }
   ngOnInit() {
-    this.getQuery();
+    if (this.queryExist) {
+        this.key = this.querySet[0];
+        this.val = this.querySet[1];
+        
+    }
+    
+    
   }
 
 }
